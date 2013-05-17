6f119f6eadf4ac7137ffbb7538c922254b3cf384
Selina Chen <selinajchen@gmail.com>
2013-05-17 03:23:48 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Archfolio::Application.config.secret_token = '363a737b1b7b07837df47bc21ad902122a821b60bf2606f040c78e4660d0bc4aa263dbf71a3579286a3dc9e4b4548e92655644493c6461e2986d708544ec329c'
